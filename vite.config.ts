3eda37ba21d8d5a5a7feca6d7f767e6afcf76c6c
maxmax <maxbakaiev@gmail.com>
2024-02-22 12:42:03 +0200

    gh Pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    ghPages(),
   ],
+  base: '/resume-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
